8fdd0068c1024f0cd708cb8f09b09150ad9ded02
Elmira2505 <63305538+Elmira2505@users.noreply.github.com>
2020-10-03 23:29:46 +0500

    Update index.js
    add function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
 
 exports.min = function min (array) {
-    if (array.length==0){
-  return 0;
+
+  if (array == undefined) {
+    return 0;
+  } else if (array.length !== 0){
+    return Math.min(...array);
+    /*
+   let result=0;
+    for (i = 0; i < array.length; i++){
+        result = result < array[i] ? result : array[i];
+    }
+    return result;
+    */
     } else {
-    return Math.min(array);}
-    
+    return 0;
+  } 
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length !== 0){
+    return Math.max(...array);
+   /* let result=0;
+    for (i = 0; i < array.length; i++){
+        result = result > array[i] ? result : array[i];
+    }
+    return result;
+    */
+  } else {
+    return 0;
+  }
+
 }
 
 exports.avg = function avg (array) {
+  if (array == undefined) {
+    return 0;
+  } else if (array.length !== 0){
+    let result=0;
+    for (i = 0; i < array.length; i++){
+        result += array[i];
+    }
+    return result/array.length;
+
+  } else {
   return 0;
+}
 }
